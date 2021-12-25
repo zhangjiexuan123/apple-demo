ba958514dd0d2d767ece8c3f4cda1477a18aae22
zhangjiexuan123 <zjx17310471043@gmail.com>
2021-12-25 23:38:29 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import { Button, Header, Container, Aside, Main, Footer } from "element-ui";
+
+Vue.use(Button);
+Vue.use(Header);
+Vue.use(Container);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
